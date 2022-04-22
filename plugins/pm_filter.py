cb114f615657cd8507caaec5835dde09ab86e54a
Anandhuyej <88794006+Anandhuyej@users.noreply.github.com>
2022-04-22 20:43:38 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -665,7 +665,7 @@ async def auto_filter(client, msg, spoll=False):
         BUTTONS[key] = search
         req = message.from_user.id if message.from_user else 0
         btn.append(
-            [ InlineKeyboardButton("ᴩᴀɢᴇ", callback_data=f"next_{req}_{key}_{off_set}"),
+            [ InlineKeyboardButton(text=f"ᴩᴀɢᴇ {round(int(total_results) / 10)}", callback_data="pages"),
                 InlineKeyboardButton(text=f"1/{round(int(total_results) / 10)}", callback_data="pages"),
              InlineKeyboardButton(text="ɴᴇxᴛ ⏩", callback_data=f"next_{req}_{key}_{offset}")]
         )
