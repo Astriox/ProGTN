7218bc7cf9da16e72b091bc14d42bb1eee63446c
Anandhuyej <88794006+Anandhuyej@users.noreply.github.com>
2022-04-24 13:04:09 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -240,9 +240,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-         reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton("⚡ᴊᴏɪɴ ɢʀᴏᴜᴘ⚡", url="https://t.me/Cinemahub182"),
-                                              InlineKeyboardButton("💥ꜱʜᴀʀᴇ💥", url="https://t.me/share/url?url=https://t.me/Cinemahub182") ],
-                                            [ InlineKeyboardButton("🔖ᴅᴏᴡɴʟᴏᴀᴅ ꜱᴜʙᴛɪᴛʟᴇ🔖", url="https://telegra.ph/httpstelegraphfilee908cfc0d08e47ca337ecjpg-01-06") ] ] )
+        
         protect_content=True if pre == 'filep' else False,
         )
                     
